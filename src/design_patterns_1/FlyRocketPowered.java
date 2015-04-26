1efe58d86108be1e769345f74757d9f9b33b2128
hrne <hrne@hrne-PC>
2015-04-26 22:31:28 +0800

    Duck

---
+++ src/design_patterns_1/FlyRocketPowered.java
@@ -0,0 +1,12 @@
+package design_patterns_1;
+
+/**
+ * ¤õ½b­¸¦æ
+ * @author hrne
+ *
+ */
+public class FlyRocketPowered implements FlyBehavior{
+	public void fly(){
+		System.out.println("I'm flying with a rocket!");
+	}
+}
