38165dcbc018ae79219299b2c3ecca03315c4d03
hrne <hrne@hrne-PC>
2015-05-03 22:53:04 +0800

    TEST1

--- src/design_patterns_2/WeatherStation.java
+++ src/design_patterns_2/WeatherStation.java
@@ -5,6 +5,6 @@ public class WeatherStation {
 		WeatherData weatherDate = new WeatherData();
 		CurrentConditionsDisplay currentDisplay= new CurrentConditionsDisplay(weatherDate);
 		
-		weatherDate.setMeasurements(80, 98, 30.4F);
+		weatherDate.setMeasurements(80, 98, 30.5F);
 	}
 }
