d16ca82f48663eb3c75091395cd922ac4ccbef1f
hrne <hrne1010@gmail.com>
2017-10-30 17:45:24 +0800

    Update Subject.java

--- src/design_patterns_2/Subject.java
+++ src/design_patterns_2/Subject.java
@@ -2,7 +2,7 @@ package design_patterns_2;
 
 
 /**
- * ¥DÃD
+ * Â¥DÃƒD
  * @author hrne
  *
  */
@@ -12,4 +12,6 @@ public interface Subject {
 	public void notifyObserver();
 	
 	int a=0;
+	
+	int b=1;
 }
